f4de85eecbf31aeb9a34741872cbb7c2b63d81c2
RupeshSangoju <rupeshbabu.sangoju@gmail.com>
2024-11-28 22:11:00 +0530

    Update .gitignore and add new project files

---
+++ test_moviepy_import.py
@@ -0,0 +1,21 @@
+'''from moviepy.editor import VideoFileClip
+print("MoviePy imported successfully!") 
+
+from PIL import Image
+import numpy
+#import pillow
+import imageio
+import imageio_ffmpeg
+import proglog
+
+print("All dependencies imported successfully!")  
+
+import sys
+print(sys.executable)  # To confirm which Python interpreter is being used
+print(sys.path)  # To verify the module search paths
+'''
+from PIL import Image
+
+print("Pillow imported successfully!")
+
+
